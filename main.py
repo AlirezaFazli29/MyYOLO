a757c4a352e9c9ec168182ebeb482d7164d78398
Alireza <alirezafazli29@gmail.com>
2024-08-26 13:13:27 +0330

    testing api

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-from fastapi import FastAPI, file, UploadFile
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+import os
+import shutil
 
 app = FastAPI()
+
+UPLOAD_DIRECTORY = "./uploaded_images"
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
 
 
 @app.get('/')
 def root():
     return {"message": "Hello World"}
+
+@app.post('/upload/')
+async def upload_file(file: UploadFile = File(...)):
+    file_location = os.path.join(UPLOAD_DIRECTORY, file.filename)
+    with open(file_location, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {'opened filename': file.filename}
+
+@app.get('/images/{filename}')
+async def get_image(filename: str):
+    file_location =os.path.join(UPLOAD_DIRECTORY, filename)
+    if os.path.exists(file_location):
+        return FileResponse(file_location)
+    else: 
+        return {"error": "File not found"}
 
 # if __name__ == "__main__":
 #     import uvicorn
