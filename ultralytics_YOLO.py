89f92de3ca7fc4e32fb7afed19dc38b5270686a8
Alireza <alirezafazli29@gmail.com>
2024-10-07 11:18:03 +0330

    cleaning the project

--- ultralytics_YOLO.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model_detect = YOLO('yolov8n.pt')
-model_segment = YOLO('yolov8n-seg.pt')
-
-results = model_detect.track(source=0, show=True)
-# results = model_detect(0, show=True)
